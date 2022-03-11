957bf0f702baa0ee7a17c4c6d1c61a295fae2ff6
Muhammad-Ilham-Ferdiansyah <ilhamferdiansyah737@gmail.com>
2022-03-12 05:52:04 +0700

    Set TailwindCSS Local, Functions and Directives

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  content: ["./index.html"],
+  darkMode: 'class',
+  theme: {
+      extend: {
+          spacing: {
+              13: '3.25rem',
+          },
+          fontFamily: {
+              inter: ['Inter']
+          },
+          colors: {
+              clifford: '#da373d',
+          },
+          animation: {
+              'spin-slow': 'spin 3s linear infinite',
+              'goyang': 'goyang 1s ease-in-out infinite'
+          },
+          keyframes: {
+              goyang: {
+                  '0%, 100%': {transform: 'rotate(-3deg)'},
+                  '50%': { transform: 'rotate(3deg)'},
+              },
+          }
+      }
+  },
+  plugins: [],
+}
